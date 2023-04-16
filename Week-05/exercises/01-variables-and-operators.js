38ab32390f514c8701da9e3a391e78792d974966
LucreciaVila <lucreciavilaf@gmail.com>
2023-04-16 19:05:00 -0300

    BaSP-W05-2023: added /week-05

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,9 @@
+console.log("--EXERCISE 1: VARIABLES AND OPERATORS");
+
+// a. crear dos variables numéricas y  y utilizar el operador suma para guardar el valor de la suma de ambos números 
+//en una 3er variable. 
+
+console.log("-Exercise 1.a:");
+
+
+
